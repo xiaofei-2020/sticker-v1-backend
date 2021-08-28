a89055238d260148a454417e228f200e006129e8
Steve <841532108@qq.com>
2021-08-28 18:22:47 +0800

    取消收藏的接口实现

--- src/controllers/collectionController.js
+++ src/controllers/collectionController.js
@@ -5,7 +5,28 @@ const collectionService = require('../services/collectionService');
  * Controller 是业务入口，由 HTTP 路由解析后调用
  */
 class CollectionController {
-
+  /**
+ * 删除一条收藏
+ * 响应格式
+ * {
+ *   success: Boolean,
+ *   code?: String, // 按照http状态码的规则, 在前面加 10, 用以区分
+ *   msg?: String,
+ *   data?: string
+ * }
+ * @param req Express 的请求参数
+ * @param res Express 的响应参数
+ */
+  async delete(req, res) {
+    // 调用 Service 层对应的业务处理方法
+    await collectionService.delete(req.params.id);
+    res.send({
+      success: true,
+      code: 10200,
+      msg: '删除成功',
+      data: ''
+    });
+  }
 }
 
 // 导出 Controller 的实例
--- src/services/collectionService.js
+++ src/services/collectionService.js
@@ -1,4 +1,4 @@
-const todoTable = require('../models/todoTable');
+const collectionTable = require('../models/collection');
 const inspirecloud = require('@byteinspire/api');
 const ObjectId = inspirecloud.db.ObjectId;
 
@@ -8,6 +8,19 @@ const ObjectId = inspirecloud.db.ObjectId;
  * Service 是业务具体实现，由 Controller 或其它 Service 调用
  */
 class CollectionService {
+  /**
+ * 删除一条收藏
+ * @param id 收藏的 _id
+ * 若不存在，则抛出 404 错误
+ */
+  async delete(id) {
+    const result = await collectionTable.where({ resource_id: ObjectId(id) }).delete();
+    if (result.deletedCount === 0) {
+      const error = new Error(`collection:${id} not found`);
+      error.status = 10404;
+      throw error;
+    }
+  }
 }
 
 // 导出 Service 的实例
